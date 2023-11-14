c7ea631064bdf92754f07d20bd67ef9ed52112fa
Vishnu Dhaya <vishnu@capecomsolutions.com>
2023-11-14 23:59:03 +0530

    Layouts and header section

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.default')
+@section('title', 'Home page')
+@section('main-content')
+    <div class='text-center'>
+        <h3>Content section</h3>
+    </div>
+@endsection
---
+++ resources/views/layouts/includes/header.blade.php
@@ -0,0 +1,29 @@
+<header>
+    <nav>
+        <div>
+            <img class="nav-logo" src="https://zunocabs.com/static/media/lk.97a6f1b4caf2372aca35.png" alt="company-logo" width="120px" height="auto">
+        </div>
+        <ul class="nav">
+            <li class="nav-item nav-li" style="margin-left:92px;">
+              <a class="nav-link text-dark" href="#">Home</a>
+            </li>
+            <li class="nav-item nav-li">
+              <a class="nav-link text-dark" href="#">About Us</a>
+            </li>
+            <li class="nav-item nav-li">
+                <a class="nav-link text-dark" href="#">Zuno Driver</a>
+            </li>
+            <li class="nav-item nav-li">
+                <a class="nav-link text-dark" href="#">Live Traffic</a>
+            </li>
+            <li class="nav-item nav-li">
+                <a class="nav-link text-dark" href="#">Contact Us</a>
+                {{-- <div class="nav-button">
+                    <button>Download</button>
+                </div> --}}
+                <button class="rounded-pill text-white bg-dark nav-button">Download</button>
+            </li>
+            
+          </ul>
+    </nav>
+</header>
